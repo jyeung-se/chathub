5057bf2effa5b5b4d47d0237db0930194d5d01b4
Jack Yeung <jyeung.se@gmail.com>
2023-03-26 03:48:35 -0400

    Added index.js, tested temporary fake auth with post request - OK.

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  return res.json({ username: username, secret: "SHA256..." });
+});
+
+app.listen(5000);

b3600acab2154af72e44aadb198d909f4f76bd28
Jack Yeung <jyeung.se@gmail.com>
2023-03-26 04:12:15 -0400

    Added Axios put to create users

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const {default: axios} = require("axios")
 
 const app = express();
 app.use(express.json());
@@ -7,7 +8,20 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "SHA256..." });
+
+  try {
+    const r = await axios.put(
+        "https://api.chatengine.io/users/",
+        {username: username, secret: username, first_name: username},
+        {headers: {"private-key": "a290d006-4000-44ea-905b-54da90eefa0b" }}
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+        return res.status(e.response.status).json(e.response.data);
+  }
+
+// This line is for testing if post goes through with the fake user login data
+//   return res.json({ username: username, secret: "SHA256..." });
 });
 
 app.listen(5000);
